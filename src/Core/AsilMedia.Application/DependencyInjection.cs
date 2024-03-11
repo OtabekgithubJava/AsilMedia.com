5eef304e51af102a39f98fc0a05d46dc7860345f
Otabek_coding <meliqoziyevotabek66@gmail.com>
2024-03-11 08:31:49 +0500

    Initial commit

---
+++ src/Core/AsilMedia.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using AsilMedia.Application.Services.Genres;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AsilMedia.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IGenreService, GenreService>();
+
+            return services;
+        }
+    }
+}
---
+++ src/External/AsilMedia.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AsilMedia.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsilMedia.Infrastructure1
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+            => Database.Migrate();
+
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<FilmMaker> FilmMakers { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+    }
+}
